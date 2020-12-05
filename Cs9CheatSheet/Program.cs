4019bcee0c98ed62165a897c69c11caf31118764
Franco Tiveron <franco.tiveron@gmail.com>
2020-12-05 18:07:49 +1100

    Add project files.

---
+++ Cs9CheatSheet/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Cs9CheatSheet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
