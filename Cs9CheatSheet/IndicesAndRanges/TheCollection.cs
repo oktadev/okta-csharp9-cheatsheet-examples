ea98e6f50eb752d57ad464b73aa9faecddc69f06
Franco Tiveron <franco.tiveron@gmail.com>
2020-12-12 18:22:16 +1100

    backup

---
+++ Cs9CheatSheet/IndicesAndRanges/TheCollection.cs
@@ -0,0 +1,82 @@
+﻿namespace Cs9CheatSheet.IndicesAndRanges.Slicing
+{
+    using System;
+    using System.Linq;
+    using Xunit;
+    using static TheCollection;
+
+    internal class TheCollection
+    {
+        static int milliseconds = DateTime.Now.Millisecond;
+        public static Random Rand = new Random(milliseconds);
+        public static int[] a = Enumerable.Range(0, Rand.Next(100, 1000)).Select(i => Rand.Next()).ToArray();
+    }
+
+    public class Tests
+    {
+        [Fact]
+        public void Past_end()
+        {
+            Assert.Throws<IndexOutOfRangeException>(() => a[a.Length]);
+            Assert.Throws<IndexOutOfRangeException>(() => a[^0]);
+        }
+        [Fact]
+        public void First_element()
+        {
+            Assert.Equal(a[0], a[^a.Length]);
+        }
+        [Fact]
+        public void Last_element()
+        {
+            Assert.Equal(a[a.Length - 1], a[^1]);
+        }
+        [Fact]
+        public void First_15()
+        {
+            Assert.Equal(a.Take(15), a[..15]);
+        }
+        [Fact]
+        public void Last_27()
+        {
+            Assert.Equal(a.Skip(a.Length - 27).Take(27), a[^27..]);
+        }
+        [Fact]
+        public void From_11_to_43()
+        {
+            Assert.Equal(a.Skip(11).Take(32), a[11..43]);
+        }
+        [Fact]
+        public void From_37_to_6_back()
+        {
+            Assert.Equal(a.Skip(a.Length - 37).Take(37 - 6), a[^37..^6]);
+        }
+        [Fact]
+        public void Starting_slice()
+        {
+            int to = Rand.Next(a.Length);
+            Assert.Equal(a.Take(to), a[..to]);
+        }
+        [Fact]
+        public void Ending_slice()
+        {
+            int from = Rand.Next(a.Length);
+            Assert.Equal(a.Skip(from), a[from..]);
+        }
+        [Fact]
+        public void Any_slice()
+        {
+            int from = Rand.Next(a.Length / Rand.Next(2, 4));
+            int size = Rand.Next(a.Length / Rand.Next(3, 5));
+            int to = from + size;
+            Assert.Equal(a.Skip(from).Take(size), a[from..to]);
+        }
+        [Fact]
+        public void Any_slice_back()
+        {
+            int from = Rand.Next(a.Length / Rand.Next(2, 4));
+            int size = Rand.Next(a.Length / Rand.Next(3, 5));
+            int to = from + size;
+            Assert.Equal(a.Skip(a.Length - to).Take(size), a[^to..^from]);
+        }
+    }
+}
---
+++ Cs9CheatSheet/NullCoalescing/Operators.cs
@@ -0,0 +1,104 @@
+﻿using System.Collections.Generic;
+using Xunit;
+
+namespace Cs9CheatSheet.NullCoalescing.Operators
+{
+    public class Tests
+    {
+        [Fact]
+        public void Null_coalescing_operator()
+        {
+            (object a, object b, object c, object d) = (new object(), null, new object(), null);
+
+            Assert.Equal(a, a != null ? a : c); //old
+            Assert.Equal(a, a ?? c); //new
+
+            Assert.Equal(c, b != null ? b : c); //old
+            Assert.Equal(c, b ?? c); //new
+
+            Assert.Equal(c, d != null ? d : b != null ? b : c != null ? c : a); //old
+            Assert.Equal(c, d ?? b ?? c ?? a); //new
+
+            object[] array = { a, b, c, d };
+            for(int i = 2; i < 4; i++)
+            {
+                foreach (var combination in Combinatory.Combinations(array, i))
+                {
+                    AssertCombination(combination);
+                }
+            }
+
+            void AssertCombination(object[] combination)
+            {
+                switch(combination)
+                {
+                    case object[] array when array.Length == 2:
+                        var (a, b) = (array[0], array[1]);
+                        Assert.Equal(a != null ? a : b, a ?? b);
+                        break;
+                    case object[] array when array.Length == 3:
+                        var (c, d, e) = (array[0], array[1], array[2]);
+                        Assert.Equal(c != null ? c : d != null ? d : e, c ?? d ?? e);
+                        break;
+                    case object[] array when array.Length == 4:
+                        var (f, g, h, i) = (array[0], array[1], array[2], array[3]);
+                        Assert.Equal(f != null ? f : g != null ? g : h != null ? h : i, g ?? f ?? h ?? i);
+                        break;
+                }
+            }
+        }
+        [Fact]
+        public void Null_coalescing_assignment()
+        {
+            (object a1, object b1, object c1) = (new object(), null, new object());
+            (object a2, object b2, object c2) = (new object(), null, new object());
+
+            Assert.NotNull(a1); Assert.NotNull(a2);
+            a1 = a1 != null ? a1 : c1; //old
+            a2 ??= c2; //new
+            Assert.NotNull(a1); Assert.NotNull(a2);
+            Assert.NotEqual(a1, c1); Assert.NotEqual(a2, c2);
+
+            Assert.Null(b1); Assert.Null(b2);
+            b1 = b1 != null ? b1 : c1; //old
+            b2 ??= c2; //new
+            Assert.NotNull(b1); Assert.NotNull(b2);
+            Assert.Equal(b1, c1); Assert.Equal(b2, c2);
+        }
+    }
+
+    static class Combinatory
+    {
+        public static IEnumerable<T[]> Combinations<T>(T[] array, int m)
+        {
+            T[] result = new T[m];
+            foreach (int[] j in Combinations(m, array.Length))
+            {
+                for (int i = 0; i < m; i++)
+                {
+                    result[i] = array[j[i]];
+                }
+                yield return result;
+            }
+        }
+        private static IEnumerable<int[]> Combinations(int m, int n)
+        {
+            int[] result = new int[m];
+            Stack<int> stack = new Stack<int>(m);
+            stack.Push(0);
+            while (stack.Count > 0)
+            {
+                int index = stack.Count - 1;
+                int value = stack.Pop();
+                while (value < n)
+                {
+                    result[index++] = value++;
+                    stack.Push(value);
+                    if (index != m) continue;
+                    yield return (int[])result.Clone();
+                    break;
+                }
+            }
+        }
+    }
+}
